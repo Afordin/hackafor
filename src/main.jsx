f03e462cb5fa46ea6c1b88a64100bd34f1c57552
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2023-09-11 16:04:30 +0200

    unai te quiero

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind-compat.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
