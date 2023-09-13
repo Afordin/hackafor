adbf8cf175a14b7b5520e723ae1afb3a3cf25766
JavierBalonga <javierbalonga@gmail.com>
2023-09-12 21:49:57 -0300

    feat(*) add Background

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Background() {
+  return (
+    <>
+      {/* Lines Background */}
+      <div
+        className="absolute inset-0 bg-repeat mix-blend-overlay"
+        style={{
+          background: "url('/background-lines.png')",
+          backgroundSize: "1180px",
+        }}
+      />
+      {/* Square Tile Background */}
+      <div
+        className="absolute inset-0 bg-repeat mix-blend-overlay"
+        style={{
+          background: "url('/background-squares-tile.png')",
+          backgroundSize: "256px",
+        }}
+      />
+      {/* Noise Background */}
+      <div
+        className="absolute inset-0 bg-repeat mix-blend-overlay"
+        style={{
+          background: "url('/background-noise.png')",
+          backgroundSize: "1920px",
+        }}
+      />
+    </>
+  );
+}
